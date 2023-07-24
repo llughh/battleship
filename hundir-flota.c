7fc1a3383a52fc824bec728e1480b668c5990404
Vicente David Mut Giraldo <88946644+llughh@users.noreply.github.com>
2023-07-24 20:54:38 +0200

    hundir-flota v4.0

--- hundir-flota.c
+++ hundir-flota.c
@@ -11,16 +11,22 @@
 
 #define BOARD_SIZE 10
 
+int tamanioTablero; // Variable global para almacenar el tamaño del tablero
 
-void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroContrincante[BOARD_SIZE][BOARD_SIZE], sem_t *semaphore) 
+
+void realizarDisparos(char tableroPropio[tamanioTablero][tamanioTablero], char tableroContrincante[tamanioTablero][tamanioTablero], sem_t *semaphore) 
 {
     // Generar una semilla aleatoria única basada en el ID del proceso hijo
     unsigned int seed = getpid()*10000;
     srand(seed);
 
     int juegoAcabado = 0;
-    int disparoExitoso = 1;
-    int direccionDisparo = 0;  // 1: derecha, 2: abajo, 3: izquierda, 4: arriba
+    int disparoExitoso = 0;
+    int direccionDisparo = 1;  // 1: derecha, 2: abajo, 3: izquierda, 4: arriba
+    // Comprobar si es posible estas direcciones de disparo
+    int posibleX= 0;
+    int posibleY = 0;
+    
     int x, y;  // Coordenadas del disparo
     
     // Contabilizar las veces de todados los barcos
@@ -31,7 +37,7 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
     
     char* estadoBarco = "";		// Tocado, Hundido
     
-    
+    // Saber el ultimo movimiento de este proceso
     int ultimoPID = 0;
     int ultimoX= 0;
     int ultimoY = 0;
@@ -52,9 +58,22 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 	}
 	
 	// Se mira cual fue el último disparo de este proceso en el archivo disparos.txt
-	FILE *archivoDisparos = fopen("disparos.txt", "r");
-	while (!feof(archivoDisparos)) {
-	    fscanf(archivoDisparos, "%d:%d,%d:%s\n", &pid, &ultimoX, &ultimoY, ultimoResultado);
+	FILE *archivoDisparos = fopen("disparos.txt", "r+");
+	while (fscanf(archivoDisparos, "%d:%d,%d:%s\n", &pid, &ultimoX, &ultimoY, ultimoResultado) == 4) {
+	    
+	    if (strcmp(ultimoResultado, "WINS") == 0) {
+	        printf("\n%d GAME OVER\n", getpid());
+	        
+	    	// Escribir GAME OVER en disparos.txt
+	        if (archivoDisparos != NULL) {
+		    fprintf(archivoDisparos, "%d:-1,-1:GAME OVER\n", getpid());
+	        }
+	        // Desbloquear el semáforo para escritura
+		sem_post(semaphore);
+	         
+	        // Se para este proceso
+		exit(0);
+	    }
 
 	    if (pid == getpid()) {
 		ultimoPID = pid;
@@ -63,43 +82,64 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 		strcpy(resultado, ultimoResultado);
 	    }
 	}
-    
-    	// Se especifican las cordenadas de ataque
-	if (direccionDisparo == 0) {
-	    // Coordenadas Aleatorias
-	    x = rand() % BOARD_SIZE;
-	    y = rand() % BOARD_SIZE;
-	} else if (direccionDisparo == 1) {
-	    // Hacia la derecha
-	    y++;
-	} else if (direccionDisparo == 2) {
-	    // Hacia abajo
-	    x++;
-	} else if (direccionDisparo == 3) {
-	    // Hacia la izquierda
-	    y--;
-	} else if (direccionDisparo == 4) {
-	    // Hacia arriba
-	    x--;
+	fclose(archivoDisparos);
+	
+	//printf("\nAnterior disparo: (%d, %d), %s", x, y, resultado);
+	
+	// Si el disparo anterior es tocado entonces se dispara hacia los lados
+	if (strcmp(resultado, "TOCADO") == 0) {
+		direccionDisparo = 1;
+		disparoExitoso = 0;
+		
+		while (disparoExitoso == 0) {
+		    // Hacia la derecha
+		    if (direccionDisparo == 1) {
+		        posibleX = x;
+		        posibleY = y + 1;
+		    // Hacia abajo
+		    } else if (direccionDisparo == 2) {
+			posibleX = x + 1;
+			posibleY = y;
+		    // Hacia la izquierda
+		    } else if (direccionDisparo == 3) {
+			posibleX = x;
+			posibleY = y -1;
+		    // Hacia arriba
+		    } else if (direccionDisparo == 4) {
+			posibleX = x - 1;
+			posibleY = y;
+		    } else {
+			direccionDisparo = 1;
+		    }
+		    
+		    // Si las coordenadas están fuera del tablero, Cambiamos la dirección del siguiente disparo
+		    if (posibleX < 0 || posibleX >= tamanioTablero || posibleY < 0 || posibleY >= tamanioTablero) {
+		    	direccionDisparo++;
+		    	
+		    } else {
+		        // Se sale del bucle
+		    	disparoExitoso = 1;
+		    	
+		    	x = posibleX;
+		    	y = posibleY;
+		    }
+		}
+	// Si el disparo anterior es agua o hundido el siguiente disparo es aleatorio
 	} else {
-	    direccionDisparo = 0;
-	    continue;
-	}
-
-	if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) {
-	    // Si las coordenadas están fuera del tablero, Cambiamos la dirección del siguiente disparo
-	    direccionDisparo++;
-	    continue;
+	    x = rand() % tamanioTablero;
+	    y = rand() % tamanioTablero;
 	}
 	
+	
+	// Mientras las coordenadas hayan sido elegidas con anterioridad, no se eligirán.
 	while (tableroContrincante[x][y] == '-' || tableroContrincante[x][y] == 'X') {
 	    // Coordenadas Aleatorias
-	    x = rand() % BOARD_SIZE;
-	    y = rand() % BOARD_SIZE;
+	    x = rand() % tamanioTablero;
+	    y = rand() % tamanioTablero;
 	}
 	
 
-	// Verificar si el disparo fue exitoso
+	// Verificar si el disparo fue exitoso en una fragata
 	if (tableroContrincante[x][y] == '2') {
 	    // Marcamos el disparo como exitoso en el tablero del contrincante
 	    tableroContrincante[x][y] = 'X';
@@ -107,11 +147,9 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 	    
 	    if (fragatasTocado == 2) {
 	    	estadoBarco = "HUNDIDO";
-	    	direccionDisparo = 0;	// Siguiente disparo es aleatorio
 	    	barcosHundidos++;
 	    } else {
 	    	estadoBarco = "TOCADO";
-	    	direccionDisparo = 1; 	// Siguente desparo hacia la derecha
 	    }
 	    
 	    // Escribir el disparo en el archivo de disparos
@@ -121,7 +159,7 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 		fclose(archivoDisparos);
 	    }
 	    
-	    
+	// Disparo en bombardero
 	} else if (tableroContrincante[x][y] == '3') {
 	    // Marcamos el disparo como exitoso en el tablero del contrincante
 	    tableroContrincante[x][y] = 'X';
@@ -129,11 +167,9 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 	    
 	    if (bombarderosTocado == 3) {
 	    	estadoBarco = "HUNDIDO";
-	    	direccionDisparo = 0;	// Siguiente disparo es aleatorio
 	    	barcosHundidos++;
 	    } else {
 	    	estadoBarco = "TOCADO";
-	    	direccionDisparo = 1;	// Siguente desparo hacia la derecha
 	    }
 	    
 	    // Escribir el disparo en el archivo de disparos
@@ -143,7 +179,7 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 		fclose(archivoDisparos);
 	    }
 	
-	
+	// Disparo en portaaviones
 	} else if (tableroContrincante[x][y] == '4') {
 	    // Marcamos el disparo como exitoso en el tablero del contrincante
 	    tableroContrincante[x][y] = 'X';
@@ -152,11 +188,9 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 	    
 	    if (portavionesTocado == 4) {
 	    	estadoBarco = "HUNDIDO";
-	    	direccionDisparo = 0; // Siguiente disparo es aleatorio
 	    	barcosHundidos++;
 	    } else {
 	    	estadoBarco = "TOCADO";
-	    	direccionDisparo = 1;	// Siguente desparo hacia la derecha
 	    }
 	    
 	    // Escribir el disparo en el archivo de disparos
@@ -166,35 +200,45 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 		fclose(archivoDisparos);
 	    }
 	
-	
+	// Disparo en agua
 	} else {
-      	     disparoExitoso = 0;
-      	     direccionDisparo = 0;
-      	     
 	     // Marcamos el disparo como agua en el tablero del contrincante
-	    tableroContrincante[x][y] = '-';
+	     tableroContrincante[x][y] = '-';
 	    
-	    // Escribir el disparo en el archivo de disparos
-	    FILE *archivoDisparos = fopen("disparos.txt", "a");
-	    if (archivoDisparos != NULL) {
-		fprintf(archivoDisparos, "%d:%d,%d:AGUA\n", getpid(), x, y);
-		fclose(archivoDisparos);
-	    }
-
-	    
+	     // Escribir el disparo en el archivo de disparos
+	     FILE *archivoDisparos = fopen("disparos.txt", "a");
+	     if (archivoDisparos != NULL) {
+		 fprintf(archivoDisparos, "%d:%d,%d:AGUA\n", getpid(), x, y);
+		 fclose(archivoDisparos);
+	     }
 	}
 	
 	// Si se han hundido todos los barcos, la partida acaba
-	if (barcosHundidos == 1) {
+	if (barcosHundidos == 3) {
+		printf("\n%d WINS\n", getpid());
+		
+		// Escribir el ganador en el archivo
+	        FILE *archivoDisparos = fopen("disparos.txt", "a");
+	        if (archivoDisparos != NULL) {
+		    fprintf(archivoDisparos, "%d:-1,-1:WINS\n", getpid());
+		    fclose(archivoDisparos);
+	        }
 		juegoAcabado = 1;
+		
+		// Desbloquear el semáforo
+		sem_post(semaphore);
+		 
+		// Se para este proceso
+		exit(0);
 	}
 	
 	// Desbloquear el semáforo para escritura
 	sem_post(semaphore);
 
+	// Enseñar al usuario el tablero del contricante
 	printf("\nPID %d:\n", getpid());
-	for (int i = 0; i < BOARD_SIZE; i++) {
-		for (int j = 0; j < BOARD_SIZE; j++) {
+	for (int i = 0; i < tamanioTablero; i++) {
+		for (int j = 0; j < tamanioTablero; j++) {
 		    printf("%c", tableroContrincante[i][j]);
 		}
 	printf("\n");
@@ -207,7 +251,32 @@ void realizarDisparos(char tableroPropio[BOARD_SIZE][BOARD_SIZE], char tableroCo
 
 
 
-void cargarTablero(const char* archivo, char tablero[BOARD_SIZE][BOARD_SIZE]) 
+void crearCopiaBatalla(pid_t hijo1, pid_t hijo2) {
+    // Obtener la fecha y hora actual
+    time_t tiempoActual;
+    time(&tiempoActual);
+    struct tm* fechaHora = localtime(&tiempoActual);
+
+    // Crear el nombre del archivo con el formato deseado
+    char nombreArchivo[100];
+    sprintf(nombreArchivo, "batalla_%d_vs_%d.%04d%02d%02d_%02d%02d.txt",
+            hijo1, hijo2,
+            fechaHora->tm_year + 1900, fechaHora->tm_mon + 1, fechaHora->tm_mday,
+            fechaHora->tm_hour, fechaHora->tm_min);
+
+    // Copiar el archivo de la batalla
+    FILE* archivoBatallaOriginal = fopen("disparos.txt", "r");
+    FILE* archivoBatallaCopia = fopen(nombreArchivo, "w");
+    // Código para copiar el contenido del archivo original al archivo de copia
+
+    // Cerrar los archivos después de la copia
+    fclose(archivoBatallaOriginal);
+    fclose(archivoBatallaCopia);
+}
+
+
+
+void cargarTablero(const char* archivo, char tablero[tamanioTablero][tamanioTablero]) 
 {
 
     FILE* archivoTablero = fopen(archivo, "r");
@@ -216,12 +285,10 @@ void cargarTablero(const char* archivo, char tablero[BOARD_SIZE][BOARD_SIZE])
         return;
     }
 
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
+    for (int i = 0; i < tamanioTablero; i++) {
+        for (int j = 0; j < tamanioTablero; j++) {
             fscanf(archivoTablero, " %c", &tablero[i][j]);
-            //printf("%c", tablero[i][j]);
         }
-        //printf("\n");
     }
 
     fclose(archivoTablero);
@@ -229,14 +296,58 @@ void cargarTablero(const char* archivo, char tablero[BOARD_SIZE][BOARD_SIZE])
 
 
 
+void limpiarArchivo (const char* archivo){
+	// Se limpia el archivo de disparos.txt
+	FILE* archivoDisparos = fopen(archivo, "w");
+	if (archivoDisparos == NULL) {
+	    printf("Error al abrir el archivo para limpiarlo.\n");
+	    return;
+	}
+	fclose(archivoDisparos);  // Cierra el archivo después de sobrescribirlo
+}
+
+
+
+int obtenerTamanioTablero() {
+	FILE *archivoTablero = fopen("tablero_jugador1.txt", "r");
+	if (archivoTablero == NULL) {
+		printf("Error al abrir el archivo tablero.txt\n");
+		return 0;
+	}
+
+	tamanioTablero = 0;
+	char c;
+
+	// Determinar el número de filas contando los saltos de línea
+	while ((c = fgetc(archivoTablero)) != EOF) {
+	    if (c == '\n') {
+		tamanioTablero++;
+	    }
+	}
+
+	fclose(archivoTablero);
+    	return tamanioTablero;
+}
+
+
+
 int main()
 {
-	char tableroJugador1[BOARD_SIZE][BOARD_SIZE];
-	char tableroJugador2[BOARD_SIZE][BOARD_SIZE];
+
+	// Obtener el tamaño del tablero desde el archivo
+	tamanioTablero = obtenerTamanioTablero();
+	
+	//printf("%d\n", tamanioTablero);
+    
+	char tableroJugador1[tamanioTablero][tamanioTablero];
+	char tableroJugador2[tamanioTablero][tamanioTablero];
 	
 	// Cargar tableros
 	cargarTablero("tablero_jugador1.txt", tableroJugador1);
 	cargarTablero("tablero_jugador2.txt", tableroJugador2);
+	
+	// Se limpia el archivo de disparos.txt
+	limpiarArchivo("disparos.txt");
 	
 	
 	// Crear los semáforos
@@ -258,13 +369,6 @@ int main()
 		// Código del primer proceso hijo (atacante 1)
 		realizarDisparos(tableroJugador1, tableroJugador2, semaphore);
 		
-		//printf("\nJugador 2\n");
-		//for (int i = 0; i < BOARD_SIZE; i++) {
-		//	for (int j = 0; j < BOARD_SIZE; j++) {
-		//	    printf("%c", tableroJugador2[i][j]);
-		//	}
-		//	printf("\n");
-		//}
 	
 		return 0;
 	}
@@ -278,13 +382,6 @@ int main()
 		// Código del segundo proceso hijo (atacante 2)
 		realizarDisparos(tableroJugador2, tableroJugador1, semaphore);
 		
-		//printf("\nJugador 1\n");
-		//for (int i = 0; i < BOARD_SIZE; i++) {
-		//	for (int j = 0; j < BOARD_SIZE; j++) {
-		//	    printf("%c", tableroJugador1[i][j]);
-		//	}
-		//	printf("\n");
-		//}
 		
 		return 0;
 	}
@@ -297,7 +394,8 @@ int main()
 	sem_close(semaphore);
 	sem_unlink("disparos_semaphore");
 	
-	
+	// Creamos una copia de la batalla en otro archivo
+	crearCopiaBatalla(pid1, pid2);
 
 	
 	return 0;
